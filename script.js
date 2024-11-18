0a5469082e86e9ecf12d753f78abd293b3a26f97
Lucascortizo <lucascortizo768@gmail.com>
2024-11-18 17:35:28 -0300

    página suporte e formulário prontos

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ toggleDarkModeButton.addEventListener('click', () => {
         toggleDarkModeButton.textContent = 'Modo Claro';
     }
 });
+
+
+const form = document.getElementById("form-suporte");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const assunto = document.getElementById("assunto");
+const mensagem = document.getElementById("mensagem");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    if (nome.value.trim() === "" || email.value.trim() === "" || assunto.value.trim() === "" || mensagem.value.trim() === "") {
+        alert("Por favor, preencha todos os campos antes de enviar.");
+        return;
+    }
+
+    alert(`FORMULÁRIO ENVIADO!\n\nNome: ${nome.value}\nE-mail: ${email.value}\nAssunto: ${assunto.value}\nMensagem: ${mensagem.value}`);
+
+    form.reset();
+});
+
+
